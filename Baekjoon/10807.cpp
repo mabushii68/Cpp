ab39e4b16bb8aef713000caa041feb71aabbf98d
mabushii68 <mabushii68@gmail.com>
2025-10-01 17:32:46 +0900

    2025 10 01 commit 001

--- Baekjoon/10807.cpp
+++ Baekjoon/10807.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
 
   int N;
-  int arr[100];
+  int arr[101];
   int v;
 
-  cin >> N;
+  cin >> N; // 첫째 줄
 
   for(int i = 0; i < N; i++) {
-    cin >> arr[i];
+    cin >> arr[i]; // 둘째 줄
   }
-  cin >> v;
+  cin >> v; // 셋째 줄
 
   int res = 0; 
 
@@ -24,3 +24,9 @@ int main() {
 
   cout << res;
 }
+
+// N개의 정수 -> 정수 v가 몇 개인지 구하는 프로그램
+// 첫째 줄 : 정수의 개수 N
+// 둘째 줄 : 정수가 공백으로 구분
+// 셋째 줄 : 찾으려고 하는 정수 v
+// -100 =< 입력 =< 100 
---
+++ Baekjoon/10871.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    int A[N];
+    int X;
+
+}
