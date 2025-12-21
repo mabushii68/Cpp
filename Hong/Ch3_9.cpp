fd444b7b4702555c4b69981802ba8e19bee279f1
김정준 <mabushii68@gmail.com>
2025-12-21 20:20:20 +0900

    2025 12 21 commit 002

--- Hong/Ch3_9.cpp
+++ Hong/Ch3_9.cpp
@@ -10,25 +10,61 @@ int main() {
     bool item2_flag = false;
     bool item3_flag = false;
     bool item4_flag = false;
-    // ...
+    // ... 이렇게 하지말고. unsigned char 하나로 관리 가능
 
-    unsigned char items_flag = 0;
-    cout << bitset<8>(items_flag) << endl; // 이렇게 하면 1bit로 8개의 아이템 상태 변경 가능..!!
+    const unsigned char opt0 = 1 << 0; 
+    const unsigned char opt1 = 1 << 1;
+    const unsigned char opt2 = 1 << 2;
+    const unsigned char opt3 = 1 << 3;
+    // opt4, 5, 6, 7, ...   
 
-    // event!
-    item1_flag = true;
+    cout << bitset<8>(opt0) << endl;
+    cout << bitset<8>(opt1) << endl;
+    cout << bitset<8>(opt2) << endl;
+    cout << bitset<8>(opt3) << endl;
 
-    // die! item2 los
-    item2_flag = false;
+    // unsigned char items_flag = 0;
+    // cout << bitset<8>(items_flag) << endl; // 이렇게 하면 1bit로 8개의 아이템 상태 변경 가능..!!
 
-    if (item3_flag == true) {
-        // event
+    unsigned char items_flags = 0;
+    cout << "No item" << bitset<8>(items_flags) << endl;
+
+    // item0 on
+    items_flags |= opt0;
+    cout << "Item0 obtainted : " << bitset<8>(items_flags) << endl;
+
+    // item3 on
+    items_flags |= opt3;
+    cout << "Item3 obtainted : " << bitset<8>(items_flags) << endl;
+
+    // item3 lost
+    items_flags &= ~opt3;
+    cout << "Item3 lost : " << bitset<8>(items_flags) << endl;
+
+    // has item1 ?
+    if (items_flags & opt1) {
+        cout << "Has item1" << endl;
     }
 
-    if (item3_flag == true && item4_flag == false) {
-        item3_flag = false;
-        item4_flag = true;
+
+    // obtain item2, 3
+    items_flags |= (opt2 | opt3);
+    cout << bitset<8>(opt2 | opt3) << endl;
+    cout << "Item2, 3 obtained" << bitset<8>(items_flags) << endl;
+
+    if ((items_flags & opt2) && !(items_flags & opt1)) {
+        items_flags ^= opt2;
+        items_flags ^= opt1; 
+        // items flags ^= (opt1 | opt2);
     }
+    cout << bitset<8>(items_flags) << endl;
+
+    // 예제
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    unsigned int pixel_color = 0xDAA520;
+    cout << bitset<32>(pixel_color) << endl;
+    
+    unsigned char red, green, blue;
 
     return 0;
 }
