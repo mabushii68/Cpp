9dc7338c44ea9b82d8149b0945e85d0df3c8188d
김정준 <mabushii68@gmail.com>
2026-01-21 11:42:03 +0900

    Remove .exe files and update .gitignore

--- Hong/Ch4_3.cpp
+++ Hong/Ch4_3.cpp
@@ -4,7 +4,26 @@
 
 using namespace std;
 
+namespace a {
+    int my_var(10);
+}
+
+namespace b {
+    int my_var(20);
+}
+
 int main() {
+
+    // using namespace std;
+    using std::cout;
+    using std::endl;
+
+    using namespace a;
+    using namespace b;
+
+    // cout << my_var << endl; // Amibiguity
+    cout << a::my_var << endl;
+    cout << b::my_var << endl;
 
     return 0;
 }
